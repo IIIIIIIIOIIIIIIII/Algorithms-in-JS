7b8765b3ac14c987f4096b9b2a0ed6768b463b2f
Yuri Rodnov <44642611+IIIIIIIIOIIIIIIII@users.noreply.github.com>
2019-10-05 12:43:43 +0300

    Update Bubble sorting.js

--- Bubble sorting.js
+++ Bubble sorting.js
@@ -1,8 +1,8 @@
 function bubbleSorting(arr){
     if(arr.length === 0) return 'This array is empty now';
     else{    
-        for(let i = 0; i < arr.length; i++){
-            for(let j = 0; j < arr.length; j++){
+        for(let i = 0; i < arr.length - 1; i++){
+            for(let j = 0; j < arr.length - 1; j++){
                 if(arr[j] > arr[j + 1]){
                     let temp = arr[j];
                     arr[j] = arr[j + 1];
